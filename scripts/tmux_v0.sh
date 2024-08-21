c5e2431e59e45fd8ffdbbaad92e7350a69eaa09e
Sebastian <sebastiangommel@tutanota.com>
2024-08-21 15:04:40 -0500

    v0.3 lower 4 screens with commands

--- scripts/tmux_v0.sh
+++ scripts/tmux_v0.sh
@@ -7,7 +7,7 @@
 # USAGE:            ./main_v1.sh
 # DEPENDENCIES:     No dependencies
 # LICENSE:          MIT License
-# VERSION:          0.2.0
+# VERSION:          0.3.0
 #====================================================
 
 # tmux division into 6 parts, 3x2, upper two should only take about 20%
@@ -35,15 +35,47 @@ tmux split-window -v -t 3
 # Split the bottom left pane into 2 panes horizontally
 tmux split-window -v -t 1
 
-# Sent a command to each pane
-tmux send-key -t 0 'tput lines' C-m
-tmux send-key -t 1 'tput lines' C-m
-tmux send-key -t 2 'tput lines' C-m
-tmux send-key -t 3 'tput lines' C-m
-tmux send-key -t 4 'tput lines' C-m
-tmux send-key -t 5 'tput lines' C-m
+# Define display table function
+display_table() {
 
-# Attach to the tmux session
+    if [ "$#" -ne 4 ] ; then
+        echo "Please pass 4 commands to be displayed"
+        exit 1
+    fi
+
+    argument_value_1="$1"
+    argument_value_2="$2"
+    argument_value_3="$3"
+    argument_value_4="$4"
+
+    # Define column widths
+    width1=18
+    width2=18
+
+    # Format and display the table
+
+    printf "%-${width1}s | %-${width2}s\n" "$argument_value_1" "$argument_value_2"
+    printf "+%s+%s+\n" "$(printf '%*s' "$width1" | tr ' ' '-')" "$(printf '%*s' "$width2" | tr ' ' '-')"
+    printf "%-${width1}s | %-${width2}s\n" "$argument_value_3" "$argument_value_4"
+
+}
+
+sleep 1
+
+# Sent a command to each pane
+tmux send-key -t 0 './tmux_panes_display_cmds_v1.sh menu_info' C-m
+tmux send-key -t 3 "$(cat << EOF 
+$(declare -f display_table) 
+display_table "last" "ulimit" "who" "id" 
+EOF
+)" C-m
+
+tmux send-key -t 1 './tmux_panes_display_cmds_v1.sh last' C-m
+tmux send-key -t 4 './tmux_panes_display_cmds_v1.sh ulimit' C-m
+
+tmux send-key -t 2 './tmux_panes_display_cmds_v1.sh who' C-m
+tmux send-key -t 5 './tmux_panes_display_cmds_v1.sh id' C-m
+
 tmux attach-session -t $SESSION_NAME
 
 # important commands to test etc:
