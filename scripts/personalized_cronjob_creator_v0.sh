12a0dd621e935e332a80642295a34c38d725b7a9
Sebastian <sebastiangommel@tutanota.com>
2024-09-05 22:43:11 +0200

    v0.2 ability to log cmd outputs

--- scripts/personalized_cronjob_creator_v0.sh
+++ scripts/personalized_cronjob_creator_v0.sh
@@ -1,19 +1,43 @@
 #!/bin/bash
 #====================================================
-# TITLE:            personalized_cronjob_creator_v0.1.sh
+# TITLE:            personalized_cronjob_creator_v0.2.sh
 # DESCRIPTION:      Linux System Monitor
 # AUTHOR:           Sebastian Gommel
 # DATE:             2024-09-04
-# USAGE:            ./personalized_cronjob_creator_v0.1.sh
+# USAGE:            ./personalized_cronjob_creator_v0.2.sh
 # DEPENDENCIES:     No dependencies
 # LICENSE:          MIT License
-# VERSION:          0.1.0
+# VERSION:          0.2.0
 #====================================================
 
-# Function to create a basic cron job
+# Define the base directory for the log files
+LOG_DIR="/var/log/custom_logs"
+
+# Create the directory if it does not exist
+mkdir -p $LOG_DIR
+
+# Function to log command output
+log_command() {
+    local log_file="$1"
+    local command="$2"
+    local header="$3"
+    local control_flag="$4"
+
+    if [ "$control_flag" -eq 1 ]; then
+        echo "### Log Date: $(date) ###" >> "$log_file"
+        echo -e "\n--- $header ---" >> "$log_file"
+        eval "$command" >> "$log_file"
+        echo -e "\n #########################\n" >> "$log_file"
+    fi    
+}
+
+# Function to create a basic cron job that logs command output
 create_cron_job() {
     local frequency="$1"
-    local job_command="$2"
+    local log_file="$2"
+    local command="$3"
+    local header="$4"
+    local control_flag="$5"
 
     # Generate cron expression based on a frequency
     local cron_time=""
@@ -24,8 +48,11 @@ create_cron_job() {
         *) cron_time="0 * * * *" ;;      # Default to hourly if incorrect value
     esac
 
+    # Escape single quotes for bash -c
+    local escaped_command=$(echo "$command" | sed "s/'/'\"'\"'/g")
+
     # The full job command as it would appear in the crontab
-    local full_job_command="$cron_time $job_command"
+    local full_job_command="$cron_time bash -c 'source /home/user_999/Documents/SysMonitor/main_v4_development/cronrocks_v0.sh; log_command \"$log_file\" \"$escaped_command\" \"$header\" $control_flag'"
 
     # Add the new cron job
     (crontab -l ; echo "$full_job_command") | crontab -
@@ -34,11 +61,12 @@ create_cron_job() {
 # Function to remove a cron job based on the job command
 remove_cron_job() {
     local job_command="$1"
-
+    
     # Remove existing cron jobs matching the job command
     crontab -l | grep -v -F "$job_command" | crontab -
 }
 
 # Testing usage
-#create_cron_job 1 "echo 'This is a test job'"
-remove_cron_job "echo 'This is a test job'"
+#create_cron_job 1 "/var/log/custom_logs/uptime.log" "uptime" "Uptime" 1
+remove_cron_job "uptime"
+
