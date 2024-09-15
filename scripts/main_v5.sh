1d8cdde1ab4ccf491dbe1a75dc5819d519f9070f
Sebastian <sebastiangommel@tutanota.com>
2024-09-15 21:47:31 +0200

    v5.2 extend UI to include change system command as first choice only

--- scripts/main_v5.sh
+++ scripts/main_v5.sh
@@ -7,11 +7,14 @@
 # USAGE:            ./main_v5.sh
 # DEPENDENCIES:     No dependencies
 # LICENSE:          MIT License
-# VERSION:          5.1.0
+# VERSION:          5.2.0
 #====================================================
 
-# Name of the file where seslected commands will be saved
+# Name of the file where selected commands will be saved
 FILE_NAME="user_arguments_v0.txt"
+
+# Define variable for change_system_values script 
+CHANGE_SYSTEM_VALUES_SCRIPT="./change_system_values_v3.sh"
 
 # Clear the file content before writing new data
 > $FILE_NAME
@@ -24,7 +27,10 @@ category4=("ipaddr_show" "hostnamectl" "ss_ltpn" "date" "firewalld" "/proc/net/t
 category5=("df" "iostat" "vmstat" "lsblk" "fdisk" "du" "/proc/mdstat" "/etc/fstab" "/proc/partitions")
 category6=("ftp_check" "ssh_check" "ip_check" "timezone_check" "spec._char_ip_check" "spec._char_timezone_check")
 
-options=("${category1[@]}" "${category2[@]}" "${category3[@]}" "${category4[@]}" "${category5[@]}" "${category6[@]}")
+# Handled different then other categories, only displayed on first choice
+category0=("Change System Values script")
+
+options=("${category0[@]}" "${category1[@]}" "${category2[@]}" "${category3[@]}" "${category4[@]}" "${category5[@]}" "${category6[@]}")
 
 
 # Function to display current options
@@ -35,9 +41,42 @@ show_options() {
     local threshold_width=80
 
 
-    echo "This is a program to display specific important output of chosen commands."
-    echo "Please choose 4 commands by entering its number one after the other."
+    echo "Welcome to this Linux System Monitor!"
+    echo "This tool helps you monitor crucial system details by executing specific commands you choose. You can also make adjustments and detect anomalies."   
+    echo " "
+    echo "It is divided into 4 sections:"
+    echo "Section 1: System configuration adjustments"
+    echo "Section 2: Personalized system monitor"
+    echo "Section 3: System logs Anomaly detection"
+    echo "Section 4: Personalized logs creator (through cronjobs)"
     echo " " 
+    
+    echo "----------------------------------------------"
+    echo "Section 1: "
+    echo "This first option allows you to access a menu for system configuration adjustments. These changes will become visible the next time you run the system monitor"
+    echo "It is created to create changes which will be visible when running the system monitor again."
+    echo " " 
+
+    # If no options have been selected yet, display Category 7
+    if [ ${#selected_options[@]} -eq 0 ]; then
+        echo "1) Run the system configuration change script (press 1 and enter to choose this.)" 
+    else
+        echo "Not available, only as first choice"
+    fi
+
+
+    echo " "
+    echo "This option in section 1 is only selectable as your first choice."
+    echo "[It is a feature. Ensuring that the numbering remained accurate, even when section 1 is only available as the first choice is a challenge resolved successfully."
+    echo " "
+    echo "----------------------------------------------"
+    echo "Section 2: "
+    echo "Here is the main functionality of this system monitor."
+    echo "This is a program to display specific important output of chosen commands."
+    echo " " 
+    echo "Please choose 4 commands by entering its number and enter; one after the other."
+    echo " " 
+
 
     print_category() {
         local category=("${!1}")
@@ -61,6 +100,13 @@ show_options() {
     }
     start_index=0
 
+    # Necessary so the index displayed for the first category 1 command is 2
+    # on the first choice
+    if [ ${#selected_options[@]} -eq 0 ]; then
+        start_index=1
+    fi
+
+
     echo "Category 1: Users and Groups"
     print_category category1[@]
 
@@ -76,25 +122,57 @@ show_options() {
     echo -e "\nCategory 5: Storage"
     print_category category5[@]
 
-    echo -e "\nCategory 6: Checking for anomalies"
+
+    echo " "
+    echo "----------------------------------------------"
+    echo "Section 3: "
+    echo -e "\nChoose anomalies you would like to check for."
+    echo "The dates and times of the first, divided middle "
+    echo "and last detected anomalies will be displayed."
     print_category category6[@]
  
+    echo " "
+    echo "----------------------------------------------"
+    echo "Section 4: "
+    echo "Implemented before end of september 2024"
 
     echo ""
+    echo "----------------------------------------------"
     echo "q) Exit the program."
     echo ""
     if [ ${#selected_options[@]} -gt 0 ];
+    echo "----------------------------------------------"
         then
         # Display chosen options so far
         echo -e "Chosen options so far: ${selected_options[*]}\n"
     fi
+    
+    echo "----------------------------------------------"
 }
 
 
 # Function to handle category-specific removal of options
 retrieve_and_update_options() {
+
+    # Special handling for Category 7 (only selectable as the first option)
+    if [ ${#selected_options[@]} -eq 0 ] && [ "$global_var_selection" -eq 1 ]; then
+        selected_option="$CHANGE_SYSTEM_VALUES_SCRIPT"
+        selected_options+=("$selected_option")
+        clear
+        $CHANGE_SYSTEM_VALUES_SCRIPT
+        exit 0
+    fi
+
     # Retrieve the selected option
-    selected_option="${options[$((global_var_selection -1 ))]}"
+    selected_option="${options[$((global_var_selection -0 ))]}"
+
+
+    # Added so the right choice is taken considering category 7 is taking 1)
+    if [ ${#selected_options[@]} -eq 0 ]; then
+        selected_option="${options[$((global_var_selection -1 ))]}"
+    fi
+
+
 
     # Check if the selected option is already chosen
     if [[ " ${selected_options[*]} " == *" $selected_option "* ]]; then
@@ -111,7 +189,7 @@ retrieve_and_update_options() {
     update_category_options "category6"
     
     # Rebuild the combined options array
-    options=("${category1[@]}" "${category2[@]}" "${category3[@]}" "${category4[@]}" "${category5[@]}" "${category6[@]}")
+    options=("$CHANGE_SYSTEM_VALUES_SCRIPT" "${category1[@]}" "${category2[@]}" "${category3[@]}" "${category4[@]}" "${category5[@]}" "${category6[@]}")
     
     fi
 }
@@ -139,7 +217,7 @@ update_category_options() {
 
 
 
-# Function to promt the user to select an option
+# Function to prompt the user to select an option
 get_selection() {
     local prompt="$1"
     local choice
@@ -148,12 +226,12 @@ get_selection() {
         if [[ "$choice" == "q" ]]; then
             echo "Exiting the script."
             exit 0
-        elif [[ "$choice" =~ ^[0-9]+$ ]] && ((choice >= 1 && choice <= ${#options[@]}));
+        elif [[ "$choice" =~ ^[0-9]+$ ]] && ((choice >= 1 && choice <= ${#options[@]} + 1));
         then
-            global_var_selection=($choice)
+            global_var_selection=$choice
             return 
         else
-            echo "Invalid input. Please enter a number between 1 and ${#options[@]}."
+            echo "Invalid input. Please enter a number between 1 and $(( ${#options[@]} + 1 ))."
         fi
     done
 }
@@ -209,6 +287,6 @@ for option in "${selected_options[@]}"; do
 done
 
 
-# Calling script, which open tmux panes with chosen comands
+# Calling script, which open tmux panes with chosen commands
 ./tmux_v3.sh ${selected_options[0]} ${selected_options[1]} ${selected_options[2]} ${selected_options[3]}    
 
